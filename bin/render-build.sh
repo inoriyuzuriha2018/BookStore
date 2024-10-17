36d5a7da2d072a18cbff4ea76e259ab357ab7a63
dungtx <dungtx@newit.co.jp>
2024-10-17 16:57:20 +0700

    update command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,7 @@ bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
+rails generate simple_form:install
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
